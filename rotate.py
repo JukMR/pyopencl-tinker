87357730e26b9bc9e692046e4f0ef1898f9c007a
Mateo de Mayo <mateodemayo@gmail.com>
2019-11-08 10:16:59 -0300

    Refactor rotate_image and show_image

--- rotate.py
+++ rotate.py
@@ -3,31 +3,40 @@ from numpy import sin, cos, pi
 from PIL import Image
 import matplotlib.pyplot as plt
 
-img = Image.open("imagen.png")
-source = np.asarray(img).copy()
-target = np.zeros(source.shape, dtype=np.uint8)
-target[:, :, 3] = 255
-fig, ax = plt.subplots()
+def rotate_image(filename="imagen.png", angle = pi / 4):
+    img = Image.open(filename)
+    source = np.asarray(img).copy()
+    target = np.zeros(source.shape, dtype=np.uint8)
+    target[:, :, 3] = 255
 
-def rotated(x, y, px, py, t):
-    v = np.array([x, y])
-    p = np.array([px, py])
-    rott = np.array([
-        [cos(t), -sin(t)],
-        [sin(t), cos(t)]
-    ])
-    return rott @ (v - p) + p
+    def rotated(x, y, px, py, t):
+        v = np.array([x, y])
+        p = np.array([px, py])
+        rott = np.array([
+            [cos(t), -sin(t)],
+            [sin(t), cos(t)]
+        ])
+        return rott @ (v - p) + p
 
-height = source.shape[0]
-width = source.shape[1]
+    height = source.shape[0]
+    width = source.shape[1]
 
-angle = pi / 4
-for x in range(width):
-    for y in range(height):
-        ox, oy = np.rint(rotated(x, y, width / 2, height / 2, -angle)).astype(int)
-        if 0 <= ox < width and 0 <= oy < height:
-            target[y, x] = source[oy, ox]
+    for x in range(width):
+        for y in range(height):
+            ox, oy = np.rint(rotated(x, y, width / 2, height / 2, -angle)).astype(int)
+            if 0 <= ox < width and 0 <= oy < height:
+                target[y, x] = source[oy, ox]
 
-ax.imshow(target, cmap='gray')
-plt.axis('off')
-plt.show()
+    return target
+
+def show_image(image):
+    fig, ax = plt.subplots()
+    ax.imshow(image, cmap='gray')
+    plt.show()
+
+def main():
+    target = rotate_image()
+    show_image(target)
+
+if __name__ == '__main__':
+    main()
