4e6a26318e4cd48fb17bca14e4f25e043537583e
Mateo de Mayo <mateodemayo@gmail.com>
2019-11-13 15:39:19 -0300

    Add exercise 3 with double buffering

--- heat_transfer.cl
+++ heat_transfer.cl
@@ -40,8 +40,40 @@ __kernel void batch_cell_temp(
         temps[y * M + x + 1] + // Right
         temps[(y + 1) * M + x] + // Bottom
         temps[y * M + x - 1] // Left
-        ) / 5;
+      ) / 5;
       barrier(CLK_GLOBAL_MEM_FENCE);
       temps[y * M + x] = temp;
+    }
+}
+
+__kernel void batch_cell_temp_double_buffer(
+    __global double *temps0,
+    __global double *temps1,
+    __global const uchar *sources,
+    int k // TODO: Must be __private
+) {
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    int N = get_global_size(1);
+    int M = get_global_size(0);
+
+    float temp;
+    __global double *from_temps, *to_temps;
+
+    for(int i = 0; i < k; i++)
+    {
+      from_temps = i % 2 == 0 ? temps0 : temps1;
+      to_temps = i % 2 == 0 ? temps1 : temps0;
+      if (sources[y * M + x]) // It is a heat source
+        temp = from_temps[y * M + x];
+      else temp = (
+        from_temps[y * M + x] + // Current
+        from_temps[(y - 1) * M + x] + // Top
+        from_temps[y * M + x + 1] + // Right
+        from_temps[(y + 1) * M + x] + // Bottom
+        from_temps[y * M + x - 1] // Left
+      ) / 5;
+      to_temps[y * M + x] = temp;
+      barrier(CLK_GLOBAL_MEM_FENCE);
     }
 }
--- heat_transfer.py
+++ heat_transfer.py
@@ -20,46 +20,31 @@ def next_temps(temps, sources):
 
 def show_image(image, interval=0.1):
     plt.imshow(image, cmap='gray')
-    plt.pause(interval)
-    # plt.show()
+    # plt.pause(interval)
+    plt.show()
 
 def main_np():
-    k = 160
-    N = 32
-    room_temp = 10.123
-    temps = np.zeros((N + 2, N + 2)).astype(np.float64)
-    sources = np.zeros((N + 2, N + 2)).astype(np.bool8)
-
-    temps[15, :] = 15.23
-    temps[18, :] = 20.5123
-    temps[20, 10:20] = -80.5123
-
-    sources[16, 16] = True
-    temps[16, 16] = 40
-
-    sources[28, 28] = True
-    temps[28, 28] = 60
-
-    # Set borders
-    temps[0, :] = temps[-1, :] = temps[:, 0] = temps[:, -1] = room_temp
-    sources[0, :] = sources[-1, :] = sources[:, 0] = sources[:, -1] = True
+    k = 320
+    N = 30
+    temps, sources = init_temps_sources(N, k)
+    show_image(temps)
     for i in range(k):
-        show_image(temps)
         temps = next_temps(temps, sources)
+    show_image(temps)
 
-def init_temps_sources(N = 32, k = 16, room_temp = 10.123):
+def init_temps_sources(N = 32, k = 16, room_temp = 100):
     temps = np.zeros((N + 2, N + 2)).astype(np.double)
     sources = np.zeros((N + 2, N + 2)).astype(np.bool8)
 
-    temps[15, :] = 15.23
-    temps[18, :] = 20.5123
-    temps[20, 10:20] = -80.5123
+    # temps[15, :] = 15.23
+    # temps[18, :] = 20.5123
+    # temps[20, 10:20] = -80.5123
 
-    sources[16, 16] = True
-    temps[16, 16] = 40
+    sources[1, 1] = True
+    temps[1, 1] = -100
 
-    sources[28, 28] = True
-    temps[28, 28] = 60
+    # sources[28, 28] = True
+    # temps[28, 28] = 60
 
     # Set borders
     temps[0, :] = temps[-1, :] = temps[:, 0] = temps[:, -1] = room_temp
@@ -102,7 +87,7 @@ def main_gpu_iterative():
         current_temps = (current_temps + 1) % 2
 
 def main_gpu_batch_global():
-    k = 320000
+    k = 320
     N = 32
     temps, sources = init_temps_sources(N, k)
     # GPU Initialization
@@ -124,7 +109,32 @@ def main_gpu_batch_global():
     cl.enqueue_nd_range_kernel(queue, kernel, (N + 2, N + 2), (1, 1))
     cl.enqueue_copy(queue, temps, temps_d)
     show_image(temps)
-    from time import sleep; sleep(3)
+    # TODO assert
+
+def main_gpu_batch_global_double_buffer():
+    k = 320
+    N = 32
+    temps, sources = init_temps_sources(N, k)
+    # GPU Initialization
+    platform_list = cl.get_platforms()
+    devices = platform_list[0].get_devices(device_type=cl.device_type.GPU)
+    context = cl.Context(devices=devices)
+    queue = cl.CommandQueue(context) # TODO: Add profiling options
+    mf = cl.mem_flags
+    temps0_d = cl.Buffer(context, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=temps)
+    temps1_d = cl.Buffer(context, mf.READ_WRITE, size=temps.nbytes)
+    sources_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=sources)
+
+    kernel_file = open("heat_transfer.cl", "r")
+    kernel_source = kernel_file.read()
+    kernel_file.close()
+    program = cl.Program(context, kernel_source).build()
+    kernel = program.batch_cell_temp_double_buffer
+    show_image(temps)
+    kernel.set_args(temps0_d, temps1_d, sources_d, np.int32(k))
+    cl.enqueue_nd_range_kernel(queue, kernel, (N + 2, N + 2), (1, 1))
+    cl.enqueue_copy(queue, temps, temps0_d if k % 2 == 0 else temps1_d)
+    show_image(temps)
     # TODO assert
 
 
@@ -132,3 +142,4 @@ if __name__ == '__main__':
     # main_np()
     # main_gpu_iterative()
     main_gpu_batch_global()
+    # main_gpu_batch_global_double_buffer()
